cdd5182e741e0a2b40f30cd26b5f6205d49be64b
dannytoomey <dtoomey@umail.ucsb.edu>
2020-11-18 12:16:44 -0800

    moved three resources to local notebook

--- static/load_models.js
+++
@@ -1,3 +0,0 @@
-import { three } from './three.js/build/three.module.js';
-import { GLTFLoader } from './three/examples/jsm/loaders/GLTFLoader.js';
-import { OrbitControls } from './three/examples/jsm/controls/OrbitControls.js';
--- static/world/components/models/setup_model.js
+++
@@ -1,7 +0,0 @@
-function setup_model(data) {
-  const model = data.scene.children[0];
-
-  return model;
-}
-
-export { setup_model }
